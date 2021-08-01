c0c3008c2ef094fe68daf8e7d918dce8378ee03c
Sam Robbins <samrobbinsgb@gmail.com>
2021-08-01 11:50:27 +0100

    Update suggested html
    Used icon rather than favicon for svg

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mySvg
 
 const html = `
 <link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/icon.svg" type="image/svg+xml">
+<link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 <link rel="manifest" href="/manifest.webmanifest">
 `;
